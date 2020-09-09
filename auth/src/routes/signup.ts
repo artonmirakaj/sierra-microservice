0e3793354eb8f69e6c2affe7bbb8f63c8fbf843c
artonmirakaj <arton.mirakaj@commonbond.co>
2020-09-09 13:53:57 -0400

    cookies and JWT

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+import { JWT_KEY } from '../../keys';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -34,6 +36,19 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // generate JWT
+    const userJwt = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, JWT_KEY);
+
+    // store it on session object
+    req.session = {
+      jwt: userJwt
+    };
+
+    console.log('req.session.jwt: ', req.session.jwt);
 
     res.status(201).send(user);
   }
